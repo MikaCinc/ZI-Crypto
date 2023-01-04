9509e0c9c51869315abc2365af4686ede4d5d587
MikaCinc <mihajlo.ls00@outlook.com>
2023-01-04 02:03:17 +0100

    progres do 4.1.2023.

---
+++ A5-1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Numerics;
+using System.ComponentModel;
+using System.Security.Policy;
+
+namespace _17743_Mihajlo_Marjanovic_ZI_Projekat
+{
+    class A5_1
+    {
+        int[] key = null;
+
+        int[] XOperationMembers = null;
+        int[] YOperationMembers = null;
+        int[] ZOperationMembers = null;
+
+        int SIZEX = 19;
+        int SIZEY = 22;
+        int SIZEZ = 23;
+
+        int[] x = null;
+        int[] y = null;
+        int[] z = null;
+
+        public A5_1(int[] newKey)
+        {
+            XOperationMembers = new int[] { 13, 16, 17, 18 };
+            YOperationMembers = new int[] { 20, 21 };
+            ZOperationMembers = new int[] { 7, 20, 21, 22 };
+
+            x = new int[SIZEX];
+            y = new int[SIZEY];
+            z = new int[SIZEZ];
+
+            this.ReloadKey(newKey);
+        }
+
+        public void ReloadKey(int[] newKey = null)
+        {
+            if (newKey != null)
+            {
+                key = newKey;
+            }
+
+            int i = 0;
+            int j = 0;
+
+            while (i < 19)
+            {
+                x[j] = key[i];
+                i++;
+                j++;
+            }
+            j = 0;
+            while (i < 41)
+            {
+                y[j] = key[i];
+                i++;
+                j++;
+            }
+            j = 0;
+            while (i < 64)
+            {
+                z[j] = key[i];
+                i++;
+                j++;
+            }
+        }
+
+        public void RegisterSteps(ref int[] sr, int[] OperationMembers)
+        {
+            int t = 0;
+            foreach (int index in OperationMembers)
+            {
+                t ^= sr[index];
+            }
+
+            Shift(ref sr, t);
+        }
+
+        public int Shift(ref int[] reg, int newEntry)
+        {
+            int ret = reg[reg.Length - 1];
+
+            for (int i = reg.Length - 1; i > 0; i--)
+            {
+                reg[i] = reg[i - 1];
+            }
+
+            reg[0] = newEntry;
+            return ret;
+        }
+
+        public int[] Crypt(int[] source, bool shouldReload = true)
+        {
+            int[] result = new int[source.Length];
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                int c = source[i];
+                int m = Majority(this.x[8], this.y[10], this.y[10]);
+
+                if (this.x[8] == m)
+                    RegisterSteps(ref x, XOperationMembers);
+
+                if (this.y[10] == m)
+                    RegisterSteps(ref y, YOperationMembers);
+
+                if (this.z[10] == m)
+                    RegisterSteps(ref z, ZOperationMembers);
+
+                int s = 0;
+                s ^= Output(x) ^ Output(y) ^ Output(z);
+                result[i] = s ^ c;
+            }
+
+            if (shouldReload)
+            {
+                this.ReloadKey();
+            }
+            return result;
+        }
+
+        public int[] Decrypt(int[] source)
+        {
+            return this.Crypt(source);
+        }
+
+        public int Output(int[] reg)
+        {
+            return reg[reg.Length - 1];
+        }
+
+        private int Majority(int x, int y, int z)
+        {
+            int m = x;
+            m += y;
+            m += z;
+
+            return m > 1 ? 1 : 0;
+        }
+
+        // function to generate small binary file which will be used in EncryptFile function as input
+        public void GenerateTestBinaryFile()
+        {
+            FileStream fs = new FileStream("a5_1_input.bin", FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            for (int i = 0; i < 3; i++)
+            {
+                // write random bit
+                bw.Write((byte)1);
+            }
+
+            bw.Close();
+            fs.Close();
+        }
+
+        // function to encrypt file using A5/1 algorithm
+        public void EncryptFile(string inputFilePath, string outputFilePath)
+        {
+            FileStream fs = new FileStream(inputFilePath, FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+
+            FileStream fs2 = new FileStream(outputFilePath, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs2);
+
+            int[] input = new int[8];
+            int[] output = new int[8];
+
+            while (br.BaseStream.Position != br.BaseStream.Length)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    if (br.BaseStream.Position == br.BaseStream.Length)
+                        break;
+                    else
+                        input[i] = br.ReadBoolean() ? 1 : 0;
+                }
+
+                output = this.Crypt(input, false);
+
+                // print input and output
+
+                //Console.Write("Input: ");
+
+                //for (int i = 0; i < 8; i++)
+                //{
+                //    Console.Write(input[i]);
+                //}
+
+                //Console.Write(" Output: ");
+
+                //for (int i = 0; i < 8; i++)
+                //{
+                //    Console.Write(output[i]);
+                //}
+
+                //Console.WriteLine();
+
+                for (int i = 0; i < 8; i++)
+                {
+                    bw.Write(output[i] == 1 ? true : false);
+                }
+            }
+
+            br.Close();
+            fs.Close();
+
+            bw.Close();
+            fs2.Close();
+            this.ReloadKey();
+        }
+
+        public void DecryptFile(string inputFilePath, string outputFilePath)
+        {
+            this.ReloadKey();
+            EncryptFile(inputFilePath, outputFilePath);
+        }
+
+
+        //Metoda CreateBMPImage čita podatke sa 24-bitne bitmap slike i koristi metod Cript klase A5_1 za šifrovanje podataka.Zatim kreira novu 24-bitnu bitmap sliku od šifrovanih podataka.
+        //Metod počinje učitavanjem ulazne slike u Bitmap objekat i dobijanjem njegove širine i visine. Zatim kreira niz podataka za skladištenje podataka o pikselima.
+        //Zatim, metoda ponavlja svaki piksel na slici i izdvaja njene komponente crvene, zelene i plave boje. Zatim čuva ove komponente u nizu podataka kao pojedinačne bitove. 
+        //Na primer, crvena komponenta piksela je uskladištena u prvih 8 elemenata podataka, 
+        //zelena komponenta je uskladištena u sledećih 8 elemenata, 
+        //a plava komponenta je uskladištena u poslednjih 8 elemenata.
+        //Nakon što su svi podaci o pikselima ekstraktovani i uskladišteni u podacima, 
+        //metoda poziva metodu Cript da šifruje podatke.
+        //Zatim kreira novi Bitmap objekat i ponovo prelazi preko svakog piksela na slici, ovog puta izdvajajući šifrovane komponente boje iz podataka i postavljajući boje piksela u izlaznoj slici.
+        //Konačno, izlazna slika se čuva u datoteci pod nazivom `A5_1_output.bmp
+        public void CreateBMPImage(string filePath)
+        {
+            // Load the BMP image into a Bitmap object
+            Bitmap bmp = (Bitmap)Image.FromFile(filePath);
+
+            // Get the width and height of the image
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            // Create a BigInteger to store the data
+            int[] data = new int[width * height * 24];
+
+            // Iterate over each pixel in the image
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // Get the pixel at the current position
+                    Color pixel = bmp.GetPixel(x, y);
+
+                    // Extract the red, green, and blue components of the pixel
+                    byte r = pixel.R;
+                    byte g = pixel.G;
+                    byte b = pixel.B;
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        data[y * width * 24 + x * 24 + i] = (r >> (7 - i)) & 1; // & 1 je maska za poslednji bit trenutnog shiftovanja u desno // ponavlja se 8 puta za ceo bajt
+                        data[y * width * 24 + x * 24 + 8 + i] = (g >> (7 - i)) & 1;
+                        data[y * width * 24 + x * 24 + 16 + i] = (b >> (7 - i)) & 1;
+                    }
+                }
+            }
+
+            // Enkriptujemo trenutne podatke
+            Console.WriteLine("bmp - message: " + data);
+            data = this.Crypt(data);
+            Console.WriteLine("bmp - enkriptovanje... " + data);
+
+            // Create a new Bitmap to store the converted image
+            Bitmap output = new Bitmap(width, height);
+
+            // Iterate over each pixel in the image
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // Extract the red, green, and blue components of the pixel from the data array
+                    byte r = 0;
+                    byte g = 0;
+                    byte b = 0;
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        r |= (byte)(data[y * width * 24 + x * 24 + i] << (7 - i));
+                        g |= (byte)(data[y * width * 24 + x * 24 + 8 + i] << (7 - i));
+                        b |= (byte)(data[y * width * 24 + x * 24 + 16 + i] << (7 - i));
+                    }
+
+                    // Set the pixel at the current position to the extracted color
+                    output.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            // Save the output image
+            output.Save("A5_1_output.bmp", ImageFormat.Bmp);
+
+            // Enkriptujemo trenutne podatke
+            data = Decrypt(data);
+            Console.WriteLine("bmp - dekriptovanje... " + data);
+
+            // Create a new Bitmap to store the converted image
+            output = new Bitmap(width, height);
+
+            // Iterate over each pixel in the image
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // Extract the red, green, and blue components of the pixel from the BigInteger
+                    byte r = 0;
+                    byte g = 0;
+                    byte b = 0;
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        r |= (byte)(data[y * width * 24 + x * 24 + i] << (7 - i));
+                        g |= (byte)(data[y * width * 24 + x * 24 + 8 + i] << (7 - i));
+                        b |= (byte)(data[y * width * 24 + x * 24 + 16 + i] << (7 - i));
+                    }
+
+                    // Set the pixel at the current position to the extracted color
+                    output.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            // Save the output image
+            output.Save("A5_1_decrypted_image.bmp", ImageFormat.Bmp);
+        }
+    }
+}
---
+++ RSA.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Image = System.Drawing.Image;
+
+namespace _17743_Mihajlo_Marjanovic_ZI_Projekat
+{
+    class RSA
+    {
+        private BigInteger _e;
+        private BigInteger _n;
+        private BigInteger _d;
+        private BigInteger _phi;
+
+        public RSA(int keySize)
+        {
+            // Generate prime numbers p and q
+            BigInteger p = GeneratePrime(keySize / 2);
+            BigInteger q = GeneratePrime(keySize / 2);
+
+            // Calculate n = p * q
+            _n = BigInteger.Multiply(p, q);
+
+            // Euklidov phi je broj integera manjih ili jednakih sa N koji ne dele nijedan zajednički faktor sa N
+            // Npr: phi(8) = 4 // [1,3,5,7]
+            // phi od prostog broja P je P-1
+            // Pa je zato lako da se izračuna phi(n) koga čini proizvod dva prosta broja p i q, na sledeći način:
+            // phi = (p - 1) * (q - 1)
+            _phi = BigInteger.Multiply(BigInteger.Subtract(p, 1), BigInteger.Subtract(q, 1));
+
+            // Generate a public key e such that gcd(e, phi) = 1
+            // Neparan broj koji ne deli zajednički faktor sa phi i n
+            _e = GeneratePublicKey(_phi);
+
+            // Calculate d such that d * e = 1 mod phi
+            // d= e^-1 mod phi(n)
+            _d = ModInverse(_e, _phi);
+        }
+
+        public BigInteger PublicKey => _e;
+        public BigInteger PrivateKey => _d;
+        public BigInteger Modulus => _n;
+        public BigInteger Phi => _phi;
+
+        public RSA(BigInteger e, BigInteger n, BigInteger d, BigInteger phi)
+        {
+            // Validate the keys
+            if (e <= 1 || e >= phi)
+            {
+                throw new Exception("Invalid public key");
+            }
+            if (d <= 0 || d >= phi)
+            {
+                throw new Exception("Invalid private key");
+            }
+            if (BigInteger.GreatestCommonDivisor(e, phi) != 1)
+            {
+                throw new Exception("Invalid public key");
+            }
+            if (d * e % phi != 1)
+            {
+                throw new Exception("Invalid private key");
+            }
+
+            _e = e;
+            _n = n;
+            _d = d;
+            _phi = phi;
+        }
+
+        private BigInteger GeneratePublicKey(BigInteger phi)
+        {
+            // E je neparan broj koji ne deli zajednički faktor sa phi i n
+            // Generate a random number in the range [2, phi - 1]
+            BigInteger e = GenerateRandomInteger(2, phi - 1);
+
+            // Keep generating random numbers until a number is found such that gcd(e, phi) = 1
+            while (BigInteger.GreatestCommonDivisor(e, phi) != 1)
+            {
+                e = GenerateRandomInteger(2, phi - 1);
+            }
+
+            return e;
+        }
+
+        private BigInteger GeneratePrime(int keySize)
+        {
+            // Generate a random number of the specified key size
+            BigInteger number = GenerateRandomInteger(keySize);
+
+            // If the number is even, add 1 to make it odd
+            if (number % 2 == 0)
+            {
+                number += 1;
+            }
+
+            // Keep generating random numbers until a prime is found
+            while (!IsPrime(number))
+            {
+                number += 2;
+            }
+
+            return number;
+        }
+
+        private BigInteger GenerateRandomInteger(int keySize)
+        {
+            // Generate a random byte array of the specified key size
+            var randomBytes = new byte[keySize / 8];
+            //RNGCryptoServiceProvider is an implementation of a random number generator.
+            using (var rng = new System.Security.Cryptography.RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(randomBytes); // The array is now filled with cryptographically strong random bytes.
+            }
+
+            // Convert the byte array to a big integer
+            BigInteger number = new BigInteger(randomBytes);
+
+            // If the number is negative, negate it
+            if (number < 0)
+            {
+                number = -number;
+            }
+
+            return number;
+        }
+
+        private BigInteger GenerateRandomInteger(BigInteger min, BigInteger max)
+        {
+            // Generate a random byte array of the same size as min and max
+            var randomBytes = new byte[Math.Max(min.ToByteArray().Length, max.ToByteArray().Length)];
+            using (var rng = new System.Security.Cryptography.RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(randomBytes);
+            }
+
+            // Convert the byte array to a big integer
+            BigInteger number = new BigInteger(randomBytes);
+
+            // If the number is negative, negate it
+            if (number < 0)
+            {
+                number = -number;
+            }
+
+            // If the number is less than min, add min to it
+            if (number < min)
+            {
+                number += min;
+            }
+
+            // If the number is greater than max, subtract max from it
+            if (number > max)
+            {
+                number -= max;
+            }
+
+            return number;
+        }
+
+        private BigInteger ModInverse(BigInteger a, BigInteger b)
+        {
+            // Calculate the inverse of a mod b using the extended Euclidean algorithm
+            BigInteger b0 = b, t, q;
+            BigInteger x0 = 0, x1 = 1;
+            if (b == 1) return 1;
+            while (a > 1)
+            {
+                q = a / b;
+                t = b;
+                b = a % b; a = t;
+                t = x0;
+                x0 = x1 - q * x0;
+                x1 = t;
+            }
+            if (x1 < 0) x1 += b0;
+            return x1;
+        }
+
+        private bool IsPrime(BigInteger number)
+        {
+            // Check if the number is even
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+
+            // Check if the number is prime by trying to divide it by odd numbers in the range [3, number/2]
+            for (BigInteger i = 3; i <= BigInteger.Divide(number, 2); i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public BigInteger Encrypt(BigInteger message)
+        {
+            // c = m^e mod n
+            return BigInteger.ModPow(message, _e, _n);
+        }
+
+        public BigInteger Decrypt(BigInteger ciphertext)
+        {
+            // m = c^d mod n
+            return BigInteger.ModPow(ciphertext, _d, _n);
+        }
+        public void EncryptFile(string inputFilePath, string outputFilePath)
+        {
+            // Read the input file into a string
+            string message = File.ReadAllText(inputFilePath);
+
+            // Convert the ciphertext string to a big integer
+            BigInteger number = BigInteger.Parse(message);
+
+            // Encrypt the plaintext string
+            BigInteger cipher = Encrypt(number);
+
+            // Write the ciphertext to the output file
+            File.WriteAllText(outputFilePath, cipher.ToString());
+        }
+
+        public void DecryptFile(string inputFilePath, string outputFilePath)
+        {
+            // Read the input file into a string
+            string ciphertextString = File.ReadAllText(inputFilePath);
+
+            // Convert the ciphertext string to a big integer
+            BigInteger cipher = BigInteger.Parse(ciphertextString);
+
+            // Decrypt the ciphertext
+            BigInteger original = Decrypt(cipher);
+
+            // Write the plaintext to the output file
+            File.WriteAllText(outputFilePath, original.ToString());
+        }
+    }
+}
